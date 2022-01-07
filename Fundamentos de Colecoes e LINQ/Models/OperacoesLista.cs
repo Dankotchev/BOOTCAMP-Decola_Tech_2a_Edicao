5d1eb762881cddf647fd3e1bece432cc2975d5ea
Dankotchev <dankotchev.tech@outlook.com>
2022-01-07 14:55:29 -0300

    Finalizado 2ª Parte do Curso

---
+++ Fundamentos de Colecoes e LINQ/Models/OperacoesLista.cs
@@ -0,0 +1,39 @@
+namespace Fundamentos_de_Colecoes_e_LINQ.Models
+{
+    public class OperacoesLista
+    {
+        public void ImprimirListaString (List<string> lista)
+        {
+            Console.WriteLine($"Quantidade de Elementos na listas: {lista.Count}");
+            for (int i = 0; i < lista.Count; i++)
+            {
+                Console.WriteLine($"Índice na lista: {i} // Estado: {lista[i]}");
+            }
+            Console.WriteLine();
+        }
+
+        public void RemoverElemento (List<string> lista, string elemento)
+        {
+            Console.WriteLine($"Removendo um elemento da lista passando o elemento, neste caso {elemento}");
+            lista.Remove(elemento);
+        }
+
+        public void RemoverElementoIndice(List<string> lista, int indice)
+        {
+            Console.WriteLine($"Removendo um elemento da lista passando um valor de posiçao, neste caso {indice}");
+            lista.RemoveAt(indice);
+        }
+
+        public void AdicionarPartirString (List<string> lista, string[] array)
+        {
+            Console.WriteLine("Adicionando elementos do Array na lista.");
+            lista.AddRange(array);
+        }
+
+        public void AdicionarPorIndice (List<string> lista, int indice, string elemento)
+        {
+            Console.WriteLine($"Adiciona um elemento ({elemento}) em uma determinada posição ({indice}) dentro da lista.");
+            lista.Insert(indice, elemento);
+        }
+    }
+}
--- Fundamentos de Colecoes e LINQ/ProgramColletions.cs
+++ Fundamentos de Colecoes e LINQ/ProgramColletions.cs
@@ -1,12 +1,34 @@
+using System.Collections.Generic;
 using Fundamentos_de_Colecoes_e_LINQ.Models;
 namespace Fundamentos_de_Colecoes_e_LINQ
 {
     class Program_Colletions
     {
-        static void Main(string[] args)
+        static void Main_(string[] args)
         {
-            // Aula de Arrays
-            
+            // Aula de Listas
+            List<string> estados = new List<string>(){"Mato Grosso do Sul", "Mato Grosso", "Goiás"};
+            OperacoesLista opList = new OperacoesLista();
+            string[] estadosArray = new string[3] {"Paraná", "Santa Catarina", "Rio Grande do Sul"};
+            estados.Add("São Paulo");
+            estados.Add("Minas Gerais");
+            estados.Add("Rio de Janeiro");
+            estados.Add("Espírito Santo");
+
+            opList.ImprimirListaString(estados);
+
+            opList.RemoverElemento(estados, "Rio de Janeiro");
+            opList.ImprimirListaString(estados);
+
+            opList.RemoverElementoIndice(estados, 1);
+            opList.ImprimirListaString(estados);
+
+            opList.AdicionarPartirString(estados, estadosArray);
+            opList.ImprimirListaString(estados);
+
+            opList.AdicionarPorIndice(estados, 0, "Distrito Federal");
+            opList.ImprimirListaString(estados);
+
         } 
     }
 }
